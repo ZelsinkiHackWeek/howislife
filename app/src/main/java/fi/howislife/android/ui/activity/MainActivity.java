4993297bced38c6e7bc35af8cce965cef48ca5af
Ville Räisänen <ville.raisanen@zalando.fi>
2016-12-19 15:14:08 +0200

    Initial ui

--- app/src/main/java/fi/howislife/android/ui/activity/MainActivity.java
+++ app/src/main/java/fi/howislife/android/ui/activity/MainActivity.java
@@ -1,12 +1,17 @@
 package fi.howislife.android.ui.activity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import fi.howislife.android.R;
 import fi.howislife.android.ui.presenter.BasePresenter;
 import fi.howislife.android.ui.presenter.MainActivityPresenter;
 
 public class MainActivity extends BaseActivity implements MainActivityPresenter.MainActivityView {
+
+    @BindView(R.id.button_happy) TextView happy;
+    @BindView(R.id.button_sad) TextView sad;
 
     @Override
     public BasePresenter getPresenter() {
@@ -17,11 +22,17 @@ public class MainActivity extends BaseActivity implements MainActivityPresenter.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initView();
     }
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
+    }
+
+    private void initView() {
+        happy.setText(new String(Character.toChars(0x1F601)));
+        sad.setText(new String(Character.toChars(0x1F61E)));
     }
 
     @Override
