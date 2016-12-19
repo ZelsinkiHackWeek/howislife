e77e5b5502167f7e3f0b092bb58f84070faded5d
Jon Duran <jon.duran@zalando.fi>
2016-12-19 12:08:18 +0200

    Add rosie to project

---
+++ app/src/main/java/android/howislife/fi/howislife/HowIsLifeApplication.java
@@ -0,0 +1,11 @@
+package android.howislife.fi.howislife;
+
+import com.karumi.rosie.application.RosieApplication;
+
+/**
+ * Created by jduran on 19/12/16.
+ */
+
+public class HowIsLifeApplication extends RosieApplication {
+
+}
