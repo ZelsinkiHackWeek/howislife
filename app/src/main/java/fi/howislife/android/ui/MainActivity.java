b8dfb24f413882753d358908bfaba238d4955e72
Ville Räisänen <ville.raisanen@zalando.fi>
2016-12-19 12:28:26 +0200

    Initial ui

--- app/src/main/java/fi/howislife/android/ui/MainActivity.java
+++ app/src/main/java/fi/howislife/android/ui/MainActivity.java
@@ -1,30 +1,31 @@
 package fi.howislife.android.ui;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import com.karumi.rosie.view.RosieActivity;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
 import fi.howislife.android.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends RosieActivity {
     @BindView(R.id.button_happy) TextView happy;
     @BindView(R.id.button_sad) TextView sad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
         initView();
     }
 
-    public void initView() {
-        ButterKnife.bind(this);
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    private void initView() {
         happy.setText(new String(Character.toChars(0x1F601)));
         sad.setText(new String(Character.toChars(0x1F61E)));
-
     }
 }
