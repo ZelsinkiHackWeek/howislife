b13972a9342e0f835ea8f2c4ce78dd4747800ce1
Jon Duran <jon.duran@zalando.fi>
2016-12-21 13:35:54 +0200

    Fetch data from firebase on javascript

--- js/app.js
+++ js/app.js
@@ -1,77 +1,32 @@
-var favMovies = new Firebase('https://moviefire.firebaseio.com/movies');
+var favMovies = new Firebase('https://question-of-the-day-96172.firebaseio.com/questions');
 
-function saveToList(event) {
-    if (event.which == 13 || event.keyCode == 13) { // as the user presses the enter key, we will attempt to save the data
-        var movieName = document.getElementById('movieName').value.trim();
-        if (movieName.length > 0) {
-            saveToFB(movieName);
-        }
-        document.getElementById('movieName').value = '';
-        return false;
-    }
+function refreshUI(questionText, oneStar, twoStars, threeStars, fourStars) {
+    var lis ='';
+//    var lis = '';
+//    for (var i = 0; i < list.length; i++) {
+//        lis += '<li data-key="' + list[i].key + '">' + list[i].name + ' [' + genLinks(list[i].key, list[i].name) + ']</li>';
+//    };
+//    document.getElementById('favMovies').innerHTML = lis;
 };
-
-function saveToFB(movieName) {
-    // this will save data to Firebase
-    favMovies.push({
-        name: movieName
-    });
-};
-
-function refreshUI(list) {
-    var lis = '';
-    for (var i = 0; i < list.length; i++) {
-        lis += '<li data-key="' + list[i].key + '">' + list[i].name + ' [' + genLinks(list[i].key, list[i].name) + ']</li>';
-    };
-    document.getElementById('favMovies').innerHTML = lis;
-};
-
-function genLinks(key, mvName) {
-    var links = '';
-    links += '<a href="javascript:edit(\'' + key + '\',\'' + mvName + '\')">Edit</a> | ';
-    links += '<a href="javascript:del(\'' + key + '\',\'' + mvName + '\')">Delete</a>';
-    return links;
-};
-
-function edit(key, mvName) {
-    var movieName = prompt("Update the movie name", mvName); // to keep things simple and old skool :D
-    if (movieName && movieName.length > 0) {
-        // build the FB endpoint to the item in movies collection
-        var updateMovieRef = buildEndPoint(key);
-        updateMovieRef.update({
-            name: movieName
-        });
-    }
-}
-
-function del(key, mvName) {
-    var response = confirm("Are certain about removing \"" + mvName + "\" from the list?");
-    if (response == true) {
-        // build the FB endpoint to the item in movies collection
-        var deleteMovieRef = buildEndPoint(key);
-        deleteMovieRef.remove();
-    }
-}
-
-function buildEndPoint (key) {
-	return new Firebase('https://moviefire.firebaseio.com/movies/' + key);
-}
 
 // this will get fired on inital load as well as when ever there is a change in the data
 favMovies.on("value", function(snapshot) {
+
     var data = snapshot.val();
     var list = [];
+    var text;
+    var oneStar;
+    var twoStars;
+    var threeStars;
+    var fourStars;
     for (var key in data) {
         if (data.hasOwnProperty(key)) {
-            name = data[key].name ? data[key].name : '';
-            if (name.trim().length > 0) {
-                list.push({
-                    name: name,
-                    key: key
-                })
-            }
+            text = data[key].text ? data[key].text : '';
+            oneStar = data[key].stars[0]
+            twoStars = data[key].stars[1]
+            threeStars = data[key].stars[2]
+            fourStars = data[key].stars[3]
         }
     }
-    // refresh the UI
-    refreshUI(list);
+    refreshUI(text, oneStar, twoStars, threeStars);
 });
